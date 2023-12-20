e52b2870284c391f635810591cdae3374808a9e7
Natalia Repkina <pn.88@mail.ru>
2023-12-20 14:45:01 +0300

    feat: add solution extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,25 +1,65 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Create a repeating string based on the given parameters
- *  
+ *
  * @param {String} str string to repeat
- * @param {Object} options options object 
+ * @param {Object} options options object
  * @return {String} repeating string
- * 
+ *
  *
  * @example
- * 
- * repeater('STRING', { repeatTimes: 3, separator: '**', 
+ *
+ * repeater('STRING', { repeatTimes: 3, separator: '**',
  * addition: 'PLUS', additionRepeatTimes: 3, additionSeparator: '00' })
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+  let result = "";
+  str = String(str);
+  let repeatTimes = options.repeatTimes ? options.repeatTimes : 1;
+  let separator = Object.hasOwn(options, "separator")
+    ? String(options.separator)
+    : "+";
+
+  let addition = Object.hasOwn(options, "addition")
+    ? String(options.addition)
+    : "";
+  let additionRepeatTimes = options.additionRepeatTimes
+    ? options.additionRepeatTimes
+    : 1;
+
+  let additionSeparator = Object.hasOwn(options, "additionSeparator")
+    ? String(options.additionSeparator)
+    : "|";
+
+  let fullAddition = createSubStr(
+    addition,
+    additionRepeatTimes,
+    additionSeparator
+  );
+
+  for (let i = 0; i < repeatTimes; i++) {
+    result = result + str + fullAddition;
+    if (i < repeatTimes - 1) {
+      result += separator;
+    }
+  }
+
+  return result;
+}
+function createSubStr(str, repeate, seporator) {
+  let result = "";
+  for (let i = 0; i < repeate; i++) {
+    result = result + str;
+    if (i < repeate - 1) {
+      result = result + seporator;
+    }
+  }
+  return result;
 }
 
 module.exports = {
-  repeater
+  repeater,
 };
