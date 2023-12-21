441b7c45ab5d9a7817a74543f4351cd8e45ea204
Natalia Repkina <pn.88@mail.ru>
2023-12-21 15:00:41 +0300

    feat: add solution mine-sweeper

--- src/mine-sweeper.js
+++ src/mine-sweeper.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * In the popular Minesweeper game you have a board with some mines and those cells
@@ -23,11 +23,60 @@ const { NotImplementedError } = require('../extensions/index.js');
  *  [1, 1, 1]
  * ]
  */
-function minesweeper(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function minesweeper(matrix) {
+  const setup = [];
+
+  for (let i = 0; i < matrix.length; i++) {
+    //создаем строку в игровом поле
+    setup.push([]);
+
+    for (let j = 0; j < matrix[i].length; j++) {
+      let count = 0;
+
+      if (i > 0) {
+        console.log(findCountMineiInRow(matrix, i - 1, j));
+        count += findCountMineiInRow(matrix, i - 1, j);
+      }
+      if (i < matrix.length - 1) {
+        console.log(findCountMineiInRow(matrix, i + 1, j));
+        count += findCountMineiInRow(matrix, i + 1, j);
+      }
+      //для текущей строки нужно исключить саму ячейку, вокруг которой ищем мины, поэтому не используем  findCountMineiInRow
+      if (j > 0) {
+        if (matrix[i][j - 1] === true) {
+          count++;
+        }
+      }
+      if (j < matrix[i].length - 1) {
+        if (matrix[i][j + 1] === true) {
+          count++;
+        }
+      }
+      //заполняем строку значением количества мин вокруг
+      setup[i].push(count);
+    }
+  }
+  return setup;
+}
+
+function findCountMineiInRow(matrix, i, j) {
+  let count = 0;
+  if (j > 0) {
+    if (matrix[i][j - 1] === true) {
+      count++;
+    }
+  }
+  if (matrix[i][j] === true) {
+    count++;
+  }
+  if (j < matrix[i].length - 1) {
+    if (matrix[i][j + 1] === true) {
+      count++;
+    }
+  }
+  return count;
 }
 
 module.exports = {
-  minesweeper
+  minesweeper,
 };
